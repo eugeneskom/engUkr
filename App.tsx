8cc64cd67382df1d00cadfeb4e99b0663c6dba93
Eugene <eugeneskom@gmail.com>
2023-07-26 23:28:01 +0300

    Added preposition looping though the rows when user makes a correct choice and show the completion screen at the end, removed redundant states

--- App.tsx
+++ App.tsx
@@ -17,12 +17,26 @@ const matchWordsArr: matchWordObjType[] = [
 const testWord = "hello";
 const example = { ukr: "година", eng: "hour" };
 
+const rowObj = [
+  { id: 1, sentence: "I _ my homework", preposition: "did", choices: ["did", "made", "does", "make"] },
+  { id: 2, sentence: "She went _ the park", preposition: "to", choices: ["to", "in", "at", "on"] },
+  { id: 3, sentence: "We live _ an apartment", preposition: "in", choices: ["in", "at", "on", "to"] },
+  { id: 4, sentence: "The book is _ the table", preposition: "on", choices: ["on", "under", "above", "beside"] },
+  { id: 5, sentence: "He was born _ August", preposition: "in", choices: ["in", "on", "at", "during"] },
+  { id: 6, sentence: "They arrived _ the airport", preposition: "at", choices: ["at", "to", "from", "on"] },
+  { id: 7, sentence: "The keys are _ the drawer", preposition: "in", choices: ["in", "at", "under", "between"] },
+  { id: 8, sentence: "She travels _ train", preposition: "by", choices: ["by", "with", "on", "through"] },
+  { id: 9, sentence: "The meeting is _ 3 PM", preposition: "at", choices: ["at", "in", "on", "from"] },
+  { id: 10, sentence: "He was hiding _ the bed", preposition: "under", choices: ["under", "behind", "below", "above"] },
+];
+
+
 export default function App() {
   return (
     <View style={{flexDirection:"column"}}>
       <View style={styles.container}>
         {/* <MatchWords words={matchWordsArr} /> */}
-        <Preposition/>
+        <Preposition prepRows={rowObj}/>
         {/* <WordsBuilder set={example} /> */}
       </View>
     </View>
--- components/Prepositions/Preposition.tsx
+++ components/Prepositions/Preposition.tsx
@@ -1,12 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { FlatList, Text, View, TouchableOpacity } from "react-native";
-
-const rowObj = [
-  { id: 1, sentence: "I _ my homework", preposition: "did", choices: ["did", "made", "does", "make"] },
-  { id: 2, sentence: "She went _ the park", preposition: "to", choices: ["to", "in", "at", "on"] },
-  { id: 3, sentence: "We live _ an apartment", preposition: "in", choices: ["in", "at", "on", "to"] },
-];
 
 type preposObj = {
   id: number;
@@ -15,29 +8,42 @@ type preposObj = {
   choices: string[];
 };
 
-export default function Preposition() {
-  const [row, setRow] = useState<preposObj>(rowObj[0]);
-  const [correctAnswer, setCorrectAnser] = useState(rowObj[0].preposition);
-  const [isCorrect, setIsCorret] = useState<null | boolean>(null);
-  // useEffect(() => {
-  //   const getPrepositionExc = async () => {
-  //     const response = await axios.get("http://localhost:3000/exercise_sentences");
-  //     console.log(response.data.rows);
-  //   };
+interface PrepositionProps {
+  prepRows: preposObj[];
+}
 
-  //   // getPrepositionExc();
+export default function Preposition({ prepRows }: PrepositionProps) {
+  const [count, setCount] = useState(0);
+  const [rows, setRows] = useState<preposObj[]>(prepRows);
+  const [correctAnswer, setCorrectAnser] = useState(rows[count].preposition);
+  const [isCorrect, setIsCorrect] = useState<null | boolean>(null);
+  const [currentRow, setCurrentRow] = useState<preposObj>(prepRows[0]);
+  const [isFinished, setIsFinished] = useState(false);
 
-  //   return () => {};
-  // }, []);
+  useEffect(() => {
+    setCurrentRow(rows[count]);
+    setCorrectAnser(rows[count].preposition);
+    setIsCorrect(null);
+    setIsFinished(false);
+    return () => {
+      // Cleanup function
+    };
+  }, [count]);
 
   const selectPrepHandler = (preposition: string) => {
-    console.log(preposition);
-    const isCorrect = correctAnswer == preposition;
-    setIsCorret(isCorrect);
-    if (isCorrect) {
-      setRow((prevRow) => {
+    const rowsNumber = rows.length - 1;
+    const isCorrect = correctAnswer === preposition;
+    setIsCorrect(isCorrect);
+
+    if (isCorrect && count < rowsNumber) {
+      setCurrentRow((prevRow) => {
         return { ...prevRow, sentence: prevRow.sentence.replace("_", preposition) };
       });
+      setTimeout(() => {
+        setCount(count + 1);
+      }, 500);
+    } else if (count >= rowsNumber) {
+      setIsFinished(true);
     }
   };
 
@@ -55,12 +61,20 @@ export default function Preposition() {
   };
 
   return (
-    <View>
-      {isCorrect === false && <Text>Wrong anser!</Text>}
-      {isCorrect && <Text>Hooray, you did it!</Text>}
-      <Text>{row.sentence}</Text>
-      <FlatList data={row.choices} keyExtractor={keyExtractor} renderItem={renderItem} />
-    </View>
+    <>
+      {!isFinished ? (
+        <View>
+          {isCorrect === false && <Text>Wrong answer!</Text>}
+          {isCorrect && <Text>Hooray, you did it!</Text>}
+          <Text>{currentRow.sentence}</Text>
+          <FlatList data={currentRow.choices} keyExtractor={keyExtractor} renderItem={renderItem} />
+        </View>
+      ) : (
+        <View>
+          <Text>The task is completed!</Text>
+        </View>
+      )}
+    </>
   );
 }
 
